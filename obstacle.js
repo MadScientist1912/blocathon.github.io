0bdccd25d598b0629d0184ca9498a8ba5a2bb015
kamran296 <96657822+kamran296@users.noreply.github.com>
2022-09-15 22:22:39 +0530

    Update obstacle.js

--- obstacle.js
+++ obstacle.js
@@ -51,9 +51,9 @@ export function getObstacleRects() {
 function createObstacle() {
   const obstacle = document.createElement("img")
   obstacle.dataset.obstacle = true
-  // todo: set img drugo nft
-  obstacle.src = "imgs/obstacle.png"
-  // todo: smeni klasa nft
+  
+  obstacle.src = "obstacle.png"
+  
   obstacle.classList.add("obstacle")
   setCustomProperty(obstacle, "--left", 100)
   worldElem.append(obstacle)
